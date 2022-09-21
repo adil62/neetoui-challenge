eadece78356509340f2be8f6efdf55f19ed74082
adil.ismail <adil.ismail@entori.com>
2022-09-21 18:22:18 +0530

    Added create new note UI

--- app/javascript/src/apis/notes.js
+++ app/javascript/src/apis/notes.js
@@ -2,10 +2,14 @@ import { LIST_NOTES_DUMMY_DATA } from "components/constants";
 
 const fetch = () => Promise.resolve(LIST_NOTES_DUMMY_DATA);
 const destroy = selectedNoteIds => Promise.resolve(selectedNoteIds);
+const update = () => Promise.resolve(true);
+const create = () => Promise.resolve(true);
 
 const notesApi = {
   fetch,
   destroy,
+  update,
+  create,
 };
 
 export default notesApi;
--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -1,13 +1,47 @@
+import * as R from "ramda";
 import * as yup from "yup";
+
+export const CONTACTS = [
+  { value: 1, label: "Ronald Richards" },
+  { value: 2, label: "Jacob Jones" },
+];
+
+export const TAGS = [
+  { value: 1, label: "Getting Started" },
+  { value: 2, label: "Onboarding" },
+  { value: 2, label: "User flow" },
+  { value: 2, label: "Ux" },
+  { value: 2, label: "Bugs" },
+];
 
 export const NOTES_FORM_INITIAL_FORM_VALUES = {
   title: "",
   description: "",
+  assignedContact: null,
+  tags: [],
 };
 
 export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
   title: yup.string().required("Title is required"),
   description: yup.string().required("Description is required"),
+  assignedContact: yup
+    .object()
+    .nullable()
+    .shape({
+      label: yup.string().oneOf(R.pluck("label", CONTACTS)),
+      value: yup.number().oneOf(R.pluck("value", CONTACTS)),
+    })
+    .required("Contact is required"),
+  tags: yup
+    .array()
+    .of(
+      yup.object().shape({
+        label: yup.string().oneOf(R.pluck("label", TAGS)),
+        value: yup.number().oneOf(R.pluck("value", TAGS)),
+      })
+    )
+    .min(1, "At least one tag should be selected")
+    .required("Tag is required"),
 });
 
 export const NOTES_TABLE_COLUMN_DATA = [
